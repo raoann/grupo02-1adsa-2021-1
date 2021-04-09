d30cd6a06cd3e306a1fbf41bdb8238d4ecde2a4e
Raoann Camara <raoann.goncalves@bandtec.com.br>
2021-04-09 15:25:45 -0300

    Atualizações da modelagem e Script do SQL

--- banco_de_dados/script_do_banco.sql
+++ banco_de_dados/script_do_banco.sql
@@ -4,22 +4,22 @@ use bd_oaktruffle;
 
 CREATE TABLE tbEmpresa (
 	id_empresa INT PRIMARY KEY auto_increment
-    ,nome_empresa varchar (100)
-    ,cnpj_empresa varchar (100)
-    ,estado_empresa varchar (100)
-    ,cidade_empresa varchar (100)
-    ,bairro_empresa varchar (100)
-    ,logradouro_empresa varchar (100)
+    ,nome_empresa varchar (100) not null
+    ,cnpj_empresa varchar (100) not null
+    ,estado_empresa varchar (100) not null
+    ,cidade_empresa varchar (100) not null
+    ,bairro_empresa varchar (100) not null
+    ,logradouro_empresa varchar (100) not null
     ,cep_empresa char (10)
 );
 
 CREATE TABLE tbFuncionario (
 	id_funcionario INT PRIMARY KEY auto_increment
-    ,nome_funcionario varchar (100)
-    ,email_funcionario varchar (100)
-    ,senha_funcionario varchar (100)
-    ,dataNasc_funcionario date
-    ,cpf_funcionario varchar (15)
+    ,nome_funcionario varchar (100) not null
+    ,email_funcionario varchar (100) not null
+    ,senha_funcionario varchar (100) not null
+    ,dataNasc_funcionario date  not null
+    ,cpf_funcionario varchar (15) not null
     ,fk_empresa int 
     ,FOREIGN KEY (fk_empresa) references tbEmpresa (id_empresa)
 );
@@ -27,18 +27,22 @@ CREATE TABLE tbFuncionario (
 CREATE TABLE tbSensor (
 	id_sensor INT PRIMARY KEY auto_increment
     ,tipo_sensor varchar (30) DEFAULT 'dht11'
-    ,status_sensor VARCHAR (30)
-    ,temperatura_coletada float
-    ,umidade_coletada float
-    ,data_hora_coletado datetime default current_timestamp
+    ,status_sensor varchar(30) Check ( status_sensor ='Ativo' or status_sensor ='Inativo')
 );
 
-
+CREATE TABLE tbDadosColetados(
+	id_DadosColetados int primary key auto_increment
+	,temperatura_coletada float not null
+    ,umidade_coletada float not null
+    ,data_hora_coletada datetime default current_timestamp
+    ,fk_sensor int
+    ,foreign key (fk_sensor) REFERENCES tbSensor (id_sensor)
+    );
 
 CREATE TABLE tbQuadrante (
 	id_quadrante INT PRIMARY KEY auto_increment
     ,descricao_quadrante varchar (100)
-    ,data_intalacao datetime default current_timestamp
+    ,data_instalacao datetime default current_timestamp
     ,fk_empresa int
     ,FOREIGN KEY (fk_empresa) REFERENCES tbEmpresa (id_empresa)
     ,fk_sensor int
@@ -63,9 +67,12 @@ insert into tbFuncionario VALUES (null,'Mateus Araujo','mateus.nascimento@bandte
                                 
 insert into tbSensor (status_sensor,temperatura_coletada,umidade_coletada) VALUES ('Ativo',24,'90')
 																				 ,('Ativo',23.2,'92')
-																				 ,('inativo',0,'0')
+																				 ,('Ativo',0,'0')
 																				 ,('Ativo',21.8,'91')
-                                                                                 ,('inativo',0,'0');
+                                                                                 ,('Inativo',0,'0');
+
+select * from tbSensor;
+
 
 insert into tbQuadrante (descricao_quadrante, fk_empresa, fk_sensor) VALUES ('area_1',1,1)
 																		   ,('area_2',2,2)
@@ -73,36 +80,8 @@ insert into tbQuadrante (descricao_quadrante, fk_empresa, fk_sensor) VALUES ('ar
 																		   ,('area_4',4,4)
 																		   ,('area_5',5,5);
                                                                            
-select*from tbFuncionario;
 select*from tbEmpresa;
-
--- Verificar qual funcionario é de uma empresa especifica (nesse caso, qual é da empresa 4)
-select tbFuncionario.nome_funcionario,tbEmpresa.id_empresa from tbFuncionario
-inner join tbEmpresa
-on fk_empresa = id_empresa where id_empresa=4;
-
--- DATA INSTALAÇÃO TA DANDO ERRO
-select q.id_quadrante,q.descricao_quadrante,s.tipo_sensor,s.status_sensor,s.temperatura_coletada,s.umidade_coletada,s.data_hora_coletado from tbQuadrante as q
-inner join tbSensor as s
-on fk_sensor = id_sensor;
-
+select*from tbFuncionario;
 select*from tbSensor;
+select*from tbDadosColetados;
 select*from tbQuadrante;
-
-
-                                                                           
-                                                                           
-                                                                           
-                                                                           
-
-
-
-
-
-
-
-
-
-
-
-
